cee9499195815a918a6d1ea67537639cebbe2deb
Meirion Hughes <mhughes@geologging.com>
2016-05-18 11:09:34 +0100

    clean up app-window

--- source/app-window.ts
+++ source/app-window.ts
@@ -1,27 +1,20 @@
 import * as electron from 'electron';
 const remote = require('electron').remote;
 
+document.getElementById("min-btn").addEventListener("click", function (e) {
+  var window: Electron.BrowserWindow = remote.getCurrentWindow();
+  window.minimize();
+});
 
-let init = () => {
-
-  document.getElementById("min-btn").addEventListener("click", function (e) {
-    var window:Electron.BrowserWindow = remote.getCurrentWindow();
-    window.minimize();
-  });
-
-  document.getElementById("max-btn").addEventListener("click", function (e) {
-    var window:Electron.BrowserWindow = remote.getCurrentWindow();
+document.getElementById("max-btn").addEventListener("click", function (e) {
+  var window: Electron.BrowserWindow = remote.getCurrentWindow();
+  if(window.isMaximized())
+    window.unmaximize();
+  else
     window.maximize();
-  });
+});
 
-  document.getElementById("close-btn").addEventListener("click", function (e) {
-    var window:Electron.BrowserWindow = remote.getCurrentWindow();
-    window.close();
-  });
-};
-
-document.onreadystatechange = function () {
-  if (document.readyState == "complete") {
-    init();
-  }
-};
+document.getElementById("close-btn").addEventListener("click", function (e) {
+  var window: Electron.BrowserWindow = remote.getCurrentWindow();
+  window.close();
+});
