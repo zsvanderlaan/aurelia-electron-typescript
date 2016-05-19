91f8effa7cd499729f589b0d97d5e2053571a58d
Meirion Hughes <mhughes@geologging.com>
2016-05-19 10:30:31 +0100

    fix app window frame and events

--- source/app-window.ts
+++ source/app-window.ts
@@ -1,19 +1,31 @@
-const remote = require('electron').remote;
+export class AppWindow
+{  
+  constructor()
+  {
+    var remote = require('electron').remote;
 
-document.getElementById("min-btn").addEventListener("click", function (e) {
-  var window: Electron.BrowserWindow = remote.getCurrentWindow();
-  window.minimize();
-});
+    document.getElementById("min-btn").addEventListener("click", function (e) {
+      var window: Electron.BrowserWindow = remote.getCurrentWindow();
+      window.minimize();
+    });
 
-document.getElementById("max-btn").addEventListener("click", function (e) {
-  var window: Electron.BrowserWindow = remote.getCurrentWindow();
-  if(window.isMaximized())
-    window.unmaximize();
-  else
-    window.maximize();
-});
+    document.getElementById("max-btn").addEventListener("click", function (e) {
+      var window: Electron.BrowserWindow = remote.getCurrentWindow();
+      if(window.isMaximized())
+        window.unmaximize();
+      else
+        window.maximize();
+    });
 
-document.getElementById("close-btn").addEventListener("click", function (e) {
-  var window: Electron.BrowserWindow = remote.getCurrentWindow();
-  window.close();
-});
+    document.getElementById("close-btn").addEventListener("click", function (e) {
+      var window: Electron.BrowserWindow = remote.getCurrentWindow();
+      window.close();
+    });
+    
+    document.getElementById("help-btn").addEventListener("click", function (e) {
+      var window: Electron.BrowserWindow = remote.getCurrentWindow();
+      window.webContents.openDevTools();
+    });
+  }
+}
+var appWindow:AppWindow = new AppWindow()
