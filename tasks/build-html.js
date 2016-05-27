835c13465ce0f916f828a4a3bc3d188c2ff2c5e8
Meirion Hughes <mhughes@geologging.com>
2016-05-27 11:27:00 +0100

    use gulp-aurelia-template-lint

--- tasks/build-html.js
+++ tasks/build-html.js
@@ -1,43 +1,17 @@
+"use strict";
+
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var linter = require('gulp-aurelia-template-lint');
 
-var voidTags = [
-    'area', 'base', 'br', 'col', 'embed', 'hr', 
-    'img', 'input', 'keygen', 'link', 'meta', 
-    'param', 'source', 'track', 'wbr'];
+const parse5_1 = require('parse5');
+const stream_1 = require('stream');
 
-var checkSelfClose = function () {
-  function sanitize(file, cb) { 
-      
-    var dirty = String(file.contents);
-   
-    var matches = dirty.match(/(?:\<[\/\\\-\"\'!() a-z=.]+\/\>)/g);
-    
-    var customTags = [];
-        
-    if(matches && matches.length > 0)
-    {       
-        matches.forEach((match)=>{
-            var tag = match.match(/[a-z\-]+/)[0];
-             
-            if(voidTags.indexOf(tag) < 0)
-                customTags.push(tag);   
-        });                   
-    };
-
-    if(customTags.length > 0)
-        gutil.log('WARNING', 'found ' + customTags.length + " non-void self-closed tags in", 
-        file.path.substring(file.cwd.length, file.path.Length),
-        "tags:", customTags
-        );
-    
-    cb(null, file);
-  }
-  return require('event-stream').map(sanitize);
-}
 
 gulp.task('build-html', function () {
-    return gulp.src('source/**/*.html')
-	.pipe(checkSelfClose())
-    .pipe(gulp.dest('output'));
+    return gulp.src([
+        '!source/app-window.html',
+        'source/**/*.html'])
+        .pipe(linter())
+        .pipe(gulp.dest('output'));
 });
